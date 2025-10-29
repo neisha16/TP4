538c155fce4024bcb67329d73c8f1e137fe53885
neisha16 <lyn-chrystel.appolon@polymtl.ca>
2025-10-29 10:45:27 -0400

    change goodhash and badhash in the script

--- myscript.py
+++ myscript.py
@@ -3,5 +3,5 @@ import os
 good_hash = os.environ.get("GOOD_HASH", "commit_bon")
 bad_hash = os.environ.get("BAD_HASH", "commit_mauvais")
 
-os.system("git bisect start $badhash $goodhash")
+os.system(f"git bisect start {bad_hash} {good_hash}")
 os.system("git bisect run python manage.py test")
